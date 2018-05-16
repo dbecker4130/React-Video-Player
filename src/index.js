c9ff6234493826211be1066c805a3d49cbca33be
Daniel Becker <dbecker4130@gmail.com>
2018-05-15 21:56:29 -0700

    project set up, content renders

---
+++ src/index.js
@@ -0,0 +1,16 @@
+console.log('Helllllloo');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+
+const Index = () => {
+    return (
+        <div>
+            <h1>VIDEO</h1>
+            <video src="https://s3-us-west-2.amazonaws.com/dt-becker-portfolio/Baker-trails.mp4" autoplay loop></video>
+        </div>
+    )
+};
+
+ReactDOM.render(<Index />, document.getElementById("index"));
